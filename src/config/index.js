2259a013a996650cd2f6e42b8ae70ceaa079f270
BaraAksayeth25 <mbara4321@gmail.com>
2021-01-12 23:09:22 +0700

    chore(config): use only one file config, add LOG_PATH

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
-import configDB from "./database";
 
 dotenv.config();
 
 export default {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
-  ...configDB(),
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DIALECT: process.env.DB_DIALECT,
+  DB_HOST: process.env.DB_HOST,
+  LOG_PATH: process.env.LOG_PATH,
 };
