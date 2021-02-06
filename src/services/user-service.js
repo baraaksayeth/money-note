8c91cbc61af1156e95f598188d83a01c45d3bc93
BaraAksayeth25 <mbara4321@gmail.com>
2021-02-06 21:43:50 +0700

    chore(service): rename mehtod and class name

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,19 +1,39 @@
-class userService {
+class UserService {
   constructor(sequelize, User, logger) {
     this.sequelize = sequelize;
     this.User = User;
     this.logger = logger;
   }
 
-  async signup(data) {
+  async get(data, option = { queryManual: true }) {
+    try {
+      let result;
+      if (option.queryManual) {
+        result = await this.sequelize.query(
+          "SELECT * FROM `users` WHERE `email` = :email",
+          {
+            replacements: { email: data.email },
+            type: this.sequelize.QueryTypes.SELECT,
+          }
+        );
+      } else {
+        result = await this.User.findOne({ email: data.email });
+      }
+      return result;
+    } catch (err) {
+      throw err.message;
+    }
+  }
+
+  async create(data) {
     try {
       const [result, metadata] = await this.sequelize.query(
-        "INSERT INTO `users` (id, email, name,  password) VALUES (:id, :email, :name, :password) ",
+        "INSERT INTO `users` (`id`, `email`, `name`,  `password`, `createdAt`, `updatedAt`) VALUES (:id, :email, :name, :password, now(), now()) ",
         {
           replacements: {
             id: data.id,
             email: data.email,
-            name: data.name,
+            name: data.email,
             password: data.password,
           },
         }
@@ -25,4 +45,4 @@ class userService {
   }
 }
 
-export default userService;
+export default UserService;
