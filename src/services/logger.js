f939e64d1aaa4e8b373fc1bfed282a5ba37b6a93
BaraAksayeth25 <mbara4321@gmail.com>
2021-02-06 21:53:15 +0700

    refactor(service): class to function

--- src/services/logger.js
+++ src/services/logger.js
@@ -2,56 +2,25 @@ import winston from "winston";
 import setDate from "../helpers/date-format";
 import env from "../config";
 
-function createLogger(route, logData) {
-  return winston.createLogger({
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({
-        filename: `./app.log`,
-      }),
-    ],
-    format: winston.format.combine(
-      winston.format.printf(info => {
-        let message = `${setDate(
-          "ddd, DD MMM YYYY HH:mm:ss"
-        )} | ${info.level.toUpperCase()} | ${env.LOG_PATH} | ${
+function formatLogger() {
+  return winston.format.combine(
+    winston.format.colorize({ all: true }),
+    winston.format.printf(
+      info =>
+        `[${setDate("ddd, DD MMM YYYY HH:mm:ss")}] [${info.level}] : ${
           info.message
-        } | `;
-
-        message += info.obj ? `data: ${JSON.stringify(info.obj)} | ` : "";
-        message += logData ? `log_data: ${JSON.stringify(logData)} |` : "";
-
-        return message;
-      }),
-      winston.format.colorize()
-    ),
-  });
+        }`
+    )
+  );
 }
 
-class LoggerService {
-  constructor(route) {
-    this.route = route;
-    this.logData = null;
-    this.logger = createLogger(this.route, this.logData);
-  }
-
-  setLogData(logData) {
-    const lengthLogData = Object.keys(logData).length;
-    this.logData = lengthLogData === 0 ? null : logData;
-  }
-
-  async info(message, obj = null) {
-    this.logger.log("info", message, { obj });
-  }
-
-  async debug(message, obj = null) {
-    this.logger.log("debug", message, { obj });
-  }
-
-  async error(message, obj = null) {
-    this.logger.log("error", message, { obj });
-  }
-}
-
-const logger = new LoggerService(env.LOG_PATH);
-export default logger;
+export default winston.createLogger({
+  level: "debug",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: env.LOG_PATH,
+    }),
+  ],
+  format: winston.format.combine(winston.format.colorize(), formatLogger()),
+});
